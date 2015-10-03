814fe81280af2f42664dad145277821c6a9398c6
Sylvain Boily <sboily@avencall.com>
2015-10-02 22:01:01 -0400

    add services-list cmd

---
+++ command/services_list.go
@@ -0,0 +1,45 @@
+package command
+
+import (
+	"strings"
+)
+
+type ServicesListCommand struct {
+	Meta
+}
+
+func (c *ServicesListCommand) Help() string {
+	helpText := `
+Usage: consul-cli services-list
+
+  List local services
+`
+
+	return strings.TrimSpace(helpText)
+}
+
+func (c *ServicesListCommand) Run() int {
+	flags := c.Meta.FlagSet()
+	flags.Usage = func() { c.UI.Output(c.Help()) }
+
+	consul, err := c.Client()
+	if err != nil {
+		c.UI.Error(err.Error())
+		return 1
+	}
+
+	client := consul.Agent()
+	out, err := client.Services()
+	if err != nil {
+		c.UI.Error(err.Error())
+		return 1
+	}
+
+	c.OutputJSON(out, true)
+
+	return 0
+}
+
+func (c *ServicesListCommand) Synopsis() string {
+	return "List local service"
+}
