5f14de5a68f4be536d965f647ba77bf0a0a5dbae
Chris Aubuchon <Chris.Aubuchon@gmail.com>
2016-12-06 18:35:02 -0600

    Add `agent monitor` command

--- commands/agent.go
+++ commands/agent.go
@@ -23,6 +23,7 @@ func newAgentCommand() *cobra.Command {
 	cmd.AddCommand(newAgentLeaveCommand())
 	cmd.AddCommand(newAgentMaintenanceCommand())
 	cmd.AddCommand(newAgentMembersCommand())
+	cmd.AddCommand(newAgentMonitorCommand())
 	cmd.AddCommand(newAgentReloadCommand())
 	cmd.AddCommand(newAgentSelfCommand())
 	cmd.AddCommand(newAgentServicesCommand())
@@ -212,6 +213,46 @@ func agentMembers(cmd *cobra.Command, args []string) error {
 
 	return output(ms)
 }
+
+// Monitor functions
+
+func newAgentMonitorCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use: "monitor",
+		Short: "Streams logs from the agent",
+		Long: "Streams logs from the agent",
+		RunE: agentMonitor,
+	}
+
+	cmd.Flags().String("loglevel", "", "Log level to filter on. Default is info")
+
+	return cmd
+}
+
+func agentMonitor(cmd *cobra.Command, args[]string) error {
+	viper.BindPFlags(cmd.Flags())
+
+	client, err := newAgent()
+	if err != nil {
+		return err
+	}
+
+
+	outputChan, err := client.Monitor(
+		viper.GetString("loglevel"),
+		nil, // XXX - Set up interrupts and stop things gracefully
+		nil, // query options. No documentation on what they are used for. Leave nil for now.
+	)
+	if err != nil {
+		return err
+	}
+
+	for s := range outputChan {
+		fmt.Printf(s)
+	}
+
+	return nil
+}		
 
 // Reload functions
 
