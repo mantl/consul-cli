0c0beb926eec9afbc13ddfab951c2b969cdc746d
Sean Bryant <sbryant@github.com>
2018-10-09 23:34:29 -0700

    Discard consul test server output by default

--- commands/root_test.go
+++ commands/root_test.go
@@ -1,6 +1,8 @@
 package commands
 
 import (
+	"flag"
+	"io/ioutil"
 	"os"
 	"testing"
 
@@ -10,9 +12,17 @@ import (
 var consulTestAddr string
 
 func TestMain(m *testing.M) {
+	var consulServerOutputEnabled bool
+	flag.BoolVar(&consulServerOutputEnabled, "enable-consul-output", false, "Enables consul server output")
+
+	flag.Parse()
 	// create a test Consul server
 	consulTestServer, err := testutil.NewTestServerConfig(func(c *testutil.TestServerConfig) {
 		c.Connect = nil
+		if !consulServerOutputEnabled {
+			c.Stderr = ioutil.Discard
+			c.Stdout = ioutil.Discard
+		}
 	})
 
 	if err != nil {
